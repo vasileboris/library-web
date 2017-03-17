4502af1910064abd8084c5d2a55473cf1bc9d076
vasile boris <boris@espressoprogrammer.com>
2017-03-17 06:51:52 +0200

    Rename model with book

--- src/main/resources/public/js/views/BookView.js
+++ src/main/resources/public/js/views/BookView.js
@@ -17,20 +17,20 @@ define(function(require) {
         },
 
         initialize: function (book) {
-            this.model = book;
-            this.listenTo(this.model, 'change', this.render);
+            this.book = book;
+            this.listenTo(this.book, 'change', this.render);
         },
 
         render: function () {
-            this.$el.html(this.template(this.model.attributes));
+            this.$el.html(this.template(this.book.attributes));
             return this;
         },
         
         deleteBook: function (e) {
             //Disable link default action
             e.preventDefault();
-            //Delete model
-            this.model.destroy();
+            //Delete book
+            this.book.destroy();
             //Delete view
             this.remove();
         }
