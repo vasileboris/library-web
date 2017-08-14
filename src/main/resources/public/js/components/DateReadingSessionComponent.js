f0b98264871688bdef5eba74594cdbfa079b860f
vasile boris <boris@espressoprogrammer.com>
2017-08-14 13:56:12 +0300

    Add JSX support for date reading session

--- src/main/resources/public/js/components/DateReadingSessionComponent.js
+++ src/main/resources/public/js/components/DateReadingSessionComponent.js
@@ -21,24 +21,18 @@ class DateReadingSessionComponent extends React.Component {
     render() {
         const dateReadingSession = this.state.dateReadingSession.attributes;
 
-        const editAnchor = React.createElement('a',
-            { href: '#', className: 'edit-item', onClick:  this.editDateReadingSession.bind(this) },
-            React.createElement('img', { src: '/img/edit.png', alt: 'edit reading session', className: 'img-icon' }));
-
-        const deleteAnchor = React.createElement('a',
-            { href: '#', className: 'delete-item' },
-            React.createElement('img', { src: '/img/delete.png', alt: 'delete reading session', className: 'img-icon' }));
-
-        return React.createElement('article',
-            {
-                className: 'result-date-reading-session'
-            },
-            React.createElement('div', { className: 'message-item'}),
-            editAnchor,
-            deleteAnchor,
-            React.createElement('div', {}, `${localizer.localize('date-reading-session-last-read-page-label')} ${dateReadingSession.lastReadPage}`),
-            React.createElement('div', {}, `${localizer.localize('date-reading-session-last-read-date-label') } ${dateReadingSession.date}`),
-            React.createElement('div', {}, `${dateReadingSession.bookmark ? localizer.localize('date-reading-session-bookmark-label') + ' "' + dateReadingSession.bookmark + '"' : ''}`));
+        return <article className="result-date-reading-session">
+            <div className="message-item"></div>
+            <a href="#" className="edit-item" onClick={this.editDateReadingSession.bind(this)}>
+                <img src="/img/edit.png" alt="edit reading session" className="img-icon"/>
+            </a>
+            <a href="#" className="delete-item">
+                <img src="/img/delete.png" alt="delete reading session" className="img-icon"/>
+            </a>
+            <div>{localizer.localize('date-reading-session-last-read-page-label')} {dateReadingSession.lastReadPage}</div>
+            <div>{localizer.localize('date-reading-session-last-read-date-label')} {dateReadingSession.date}</div>
+            <div>{dateReadingSession.bookmark ? localizer.localize('date-reading-session-bookmark-label') + ' "' + dateReadingSession.bookmark + '"' : ''}</div>
+        </article>
     }
 
     editDateReadingSession(e) {
