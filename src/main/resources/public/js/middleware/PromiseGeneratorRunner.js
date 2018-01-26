c76f69c2fc7a694bc3011a55057f6d8f9f4b47b6
vasile boris <boris@espressoprogrammer.com>
2018-01-26 08:26:26 +0200

    Refactor names in PromiseGeneratorRunner

--- src/main/resources/public/js/middleware/PromiseGeneratorRunner.js
+++ src/main/resources/public/js/middleware/PromiseGeneratorRunner.js
@@ -8,7 +8,7 @@ export function run(generator, ...params) {
             const promise = result.value;
             promise
                 .then(response => iterate(iterator.next(response ? response.data : undefined)))
-                .catch(error => iterator.throw(error))
+                .catch(error => iterator.throw(error));
         }
     }
 
