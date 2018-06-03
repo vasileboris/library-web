94a7109475c254e571fc44fb7566dff615770629
vasile boris <boris@espressoprogrammer.com>
2018-06-03 13:36:44 +0300

    Handle reading session progress errors

--- src/main/resources/public/js/sagas/ReadingSessionProgressSagas.js
+++ src/main/resources/public/js/sagas/ReadingSessionProgressSagas.js
@@ -2,6 +2,7 @@ import { call, put, takeLatest } from 'redux-saga/effects';
 import { fetchReadingSessionProgress } from 'api/ReadingSessionProgressApi';
 import { receiveReadingSessionProgressAction, FETCH_READING_SESSION_PROGRESS } from 'actions/ReadingSessionProgressAction';
 import { receiveMessageAction } from 'actions/MessageAction';
+import localizer from 'utils/Localizer';
 
 function* callFetchReadingSessionProgress(action) {
     try {
@@ -12,7 +13,7 @@ function* callFetchReadingSessionProgress(action) {
         if (error.response.status === 404) {
             yield put(receiveReadingSessionProgressAction(null));
         } else {
-            yield put(receiveMessageAction(error));
+            yield put(receiveMessageAction(localizer.localize('reading-session-progress-retrieve-error', error.response.status)));
         }
     }
 }
