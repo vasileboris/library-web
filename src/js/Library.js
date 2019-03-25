47338041aa0d9daf15d014c035f7fea1145aa3d7
Vasile Boris <boris@espressoprogrammer.com>
2019-03-25 07:08:20 +0200

    Move i18next initialisation inside localizer

--- src/js/Library.js
+++ src/js/Library.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import LibraryRouter from 'routers/LibraryRouter';
-import i18next from 'i18next';
-import Fetch from 'i18next-fetch-backend';
-import PROP from 'utils/PROP';
+import localizer from 'utils/Localizer';
 
-i18next
-    .use(Fetch)
-    .init({
-        backend: {
-            loadPath: lng => {
-                const suffix = 'en' !== lng ? `_${lng}` : '';
-                return `/translations/Messages${suffix}.properties`
-            },
-            parse: data => PROP.parse(data)
-        },
-        lng: "en",
-        fallbackLng: "en",
-        interpolation: {
-            prefix: "{",
-            suffix: "}"
-        },
-        debug: true
-    }, () => {
-        render(<LibraryRouter/>, document.getElementById('app-div'));
-    });
+localizer.init(() => {
+    render(<LibraryRouter/>, document.getElementById('app-div'));
+});
--- src/js/utils/Localizer.js
+++ src/js/utils/Localizer.js
@@ -1,13 +1,37 @@
-import i18n from 'i18next';
+import i18next from 'i18next';
+import Fetch from "i18next-fetch-backend";
+import PROP from "./PROP";
 
 const Localizer = {
+
+    init: function(callback) {
+        i18next
+            .use(Fetch)
+            .init({
+                backend: {
+                    loadPath: lng => {
+                        const suffix = 'en' !== lng ? `_${lng}` : '';
+                        return `/translations/Messages${suffix}.properties`
+                    },
+                    parse: data => PROP.parse(data)
+                },
+                lng: "en",
+                fallbackLng: "en",
+                interpolation: {
+                    prefix: "{",
+                    suffix: "}"
+                },
+                debug: true
+            }, () => callback());
+
+    },
 
     localize: function (key, ...args) {
         const values = args.reduce((values, value, idx) => ({
             ...values,
             [idx]: value
         }) , {});
-        return i18n.t(key, values);
+        return i18next.t(key, values);
     }
 
 };
